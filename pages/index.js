9682e00324847b29a4e2b9b738840cc1f3c1e80c
Padmashree Jha <codewithpom@gmail.com>
2021-11-13 14:39:35 +0000

    Initialized the project

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>
+        Blog by Padmashree 
+      </title>
+      <meta name="description" content="Here I write blogs based on Computer Science"></meta>
+
+    </Head>
+    <div className="container">
+      
+      <h1>
+        Blog By Padmashree Jha
+      </h1>
+    </div>
+    </>
+  )
+}
